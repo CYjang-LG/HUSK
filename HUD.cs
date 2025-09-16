565425704f17e447b5cf2d2b49c02f962b72233c
Husk to Soul <ght90827@gmail.com>
2025-09-16 16:49:48 +0900

    Update HUD.cs

--- HUD.cs
+++ HUD.cs
@@ -22,27 +22,29 @@ public class HUD : MonoBehaviour
             case InfoType.Exp:
                 float curExp = GameManager.instance.exp;
                 float maxExp = GameManager.instance.nextExp[Mathf.Min(GameManager.instance.level, GameManager.instance.nextExp.Length - 1)];
-                mySlider.value = curExp/maxExp;
+                mySlider.value = curExp / maxExp;
                 break;
+            
             case InfoType.Level:
-                myText.text = string.Format("Lv.{0:F0}",GameManager.instance.level);
+                myText.text = string.Format("Lv.{0:F0}", GameManager.instance.level);
                 break;
+            
             case InfoType.Kill:
                 myText.text = string.Format("{0:F0}", GameManager.instance.kill);
                 break;
+            
             case InfoType.Time:
                 float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
                 int min = Mathf.FloorToInt(remainTime / 60);
                 int sec = Mathf.FloorToInt(remainTime % 60);
-                myText.text = string.Format("{0:D2}:{1:D2}", min, sec); //D2 ÀÚÀÚ¸®¼ö °íÁ¤
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
                 break;
+            
             case InfoType.Health:
-                float curhealth = GameManager.instance.health;
-                float maxHealth = GameManager.instance.maxhealth;
-                mySlider.value = curhealth/maxHealth;
-
+                float curHealth = GameManager.instance.health;
+                float maxHealth = GameManager.instance.maxHealth; // 오타 수정
+                mySlider.value = curHealth / maxHealth;
                 break;
-
         }
     }
 }
