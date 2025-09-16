c784a69ff3e7608e7d2e67498dc60dc1463c72df
Husk to Soul <ght90827@gmail.com>
2025-09-16 16:52:38 +0900

    Update LevelUp.cs

--- LevelUp.cs
+++ LevelUp.cs
@@ -11,22 +11,21 @@ public class LevelUp : MonoBehaviour
         items = GetComponentsInChildren<Item>(true);
     }
 
-    public void show()
+    public void Show() // 메서드명 일관성 개선
     {
         Next();
         rect.localScale = Vector3.one;
         GameManager.instance.Stop();
-        AudioManager.Instance.PlaySfx(AudioManager.Sfx.LevelUp);
-        AudioManager.Instance.EffectBGM(true);
-
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.LevelUp);
+        AudioManager.instance.EffectBgm(true);
     }
 
-    public void hide()
+    public void Hide() // 메서드명 일관성 개선
     {
         rect.localScale = Vector3.zero;
         GameManager.instance.Resume();
-        AudioManager.Instance.PlaySfx(AudioManager.Sfx.Select);
-        AudioManager.Instance.EffectBGM(true);
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.Select);
+        AudioManager.instance.EffectBgm(false); // 효과 해제
     }
 
     public void Select(int index)
@@ -36,27 +35,28 @@ public class LevelUp : MonoBehaviour
 
     void Next()
     {
-        //1. 모든아이템 비활성화
+        // 1. 모든 아이템 비활성화
         foreach (Item item in items)
         {
             item.gameObject.SetActive(false);
         }
-        //2. 그 중에서 랜덤하게 3개 아이템만 활성화
+        
+        // 2. 그 중에서 랜덤하게 3개 아이템만 활성화
         int[] ran = new int[3];
-        while (true) //무한반복
+        while (true)
         {
             ran[0] = Random.Range(0, items.Length);
             ran[1] = Random.Range(0, items.Length);
             ran[2] = Random.Range(0, items.Length);
 
-            if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2]) //무한반복 빠져나가기
+            if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2])
                 break;
         }
 
-        for (int index=0; index < ran.Length; index++)
+        for (int index = 0; index < ran.Length; index++)
         {
             Item ranItem = items[ran[index]];
-            //3. 만렙 아이템의 경우는 소비아이템으로 대체
+            // 3. 만렙 아이템의 경우는 소비아이템으로 대체
             if(ranItem.level == ranItem.data.damages.Length)
             {
                 items[4].gameObject.SetActive(true);
@@ -65,8 +65,6 @@ public class LevelUp : MonoBehaviour
             {
                 ranItem.gameObject.SetActive(true);
             }
-                
         }
-        
     }
 }
