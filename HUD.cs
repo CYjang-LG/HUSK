42cfacc1109d0ce50d819eaf4a63bc84c9d0e98c
Husk to Soul <ght90827@gmail.com>
2025-09-16 16:42:35 +0900

    Update HUD.cs

--- HUD.cs
+++ HUD.cs
@@ -34,7 +34,7 @@ public class HUD : MonoBehaviour
                 float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
                 int min = Mathf.FloorToInt(remainTime / 60);
                 int sec = Mathf.FloorToInt(remainTime % 60);
-                myText.text = string.Format("{0:D2}:{1:D2}", min, sec); //D2 ÀÚÀÚ¸®¼ö °íÁ¤
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec); //D2 Ã€ÃšÃ€ÃšÂ¸Â®Â¼Ã¶ Â°Ã­ÃÂ¤
                 break;
             case InfoType.Health:
                 float curhealth = GameManager.instance.health;
